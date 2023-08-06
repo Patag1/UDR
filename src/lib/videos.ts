3821c8e10fa064271d554831eded348b3b37d50a
Augusto <augustosasso97@gmail.com>
2023-08-06 17:37:37 -0300

    video update

--- src/lib/videos.ts
+++ src/lib/videos.ts
@@ -1,6 +1,6 @@
 export const videos = [
     { query: 'QXc_ozsH_cg', name: 'Harvard\'s CS50: Introduction to Computer Science (ES)', hard: false },
-    { query: '5miHyP6lExg', name: 'Vercel Full-Stack E-commerce', hard: true },
+    { query: 'a_7Z7C_JCyo', name: '15 React Projects', hard: false },
     { query: 'c_-b_isI4vg', name: 'Vercel Full-Stack Airbnb clone', hard: true },
     { query: 'mSUKMfmLAt0', name: 'Vercel Full-Stack Reddit clone', hard: true },
 ]
